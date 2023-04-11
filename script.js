36704652a27f1446f9a1925a30eecbe1970ce5b4
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-11 16:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,76 @@
-//complete this code
-class Person {
-	constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// //complete this code
+// class Person {
+// 	constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  get name() {
-    return this.name;
-  }
+//   get name() {
+//     return this.name;
+//   }
 
-  set age(age) {
-    this.age = age;
+//   set age(age) {
+//     this.age = age;
+//   }
+// }
+
+// class Student extends Person {
+// 	study() {
+//     console.log(`${this.name} is studying.`);
+//   }
+// }
+
+// class Teacher extends Person {
+// 	teach() {
+//     console.log(`${this.name} is teaching.`);
+//   }
+// }
+
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
+
+
+
+
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+  get species() {
+    return this._species;
+  }
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
   }
 }
-
-class Student extends Person {
-	study() {
-    console.log(`${this.name} is studying.`);
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
   }
 }
-
-class Teacher extends Person {
-	teach() {
-    console.log(`${this.name} is teaching.`);
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
   }
 }
-
 // Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
