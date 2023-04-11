51c9c0fda04bbd9c1730b89b508c2ca39d2aa674
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-11 19:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 //complete this code
 class Person {
-	constructor(name, age) {
-    this.name = name;
-    this.age = age;
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
   }
 
   get name() {
-    return this.name;
+    return this._name;
   }
 
   set age(age) {
-    this.age = age;
+    this._age = age;
+  }
+
+  get age() {
+    return this._age;
   }
 }
 
 class Student extends Person {
-	study() {
-    console.log(`${this.name} is studying.`);
+  study() {
+    console.log(`${this._name} is studying`);
   }
 }
 
 class Teacher extends Person {
-	teach() {
-    console.log(`${this.name} is teaching.`);
+  teach() {
+    console.log(`${this._name} is teaching`);
   }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
@@ -34,4 +37,38 @@ window.Teacher = Teacher;
 
 
 
-    
+
+// //complete this code
+// class Person {
+// 	constructor(name, age){
+// 		this._name = name;
+// 		this._age = age;
+// 	}
+// 	get name(){
+// 		return this._name;
+// 	}
+// 	set age(){
+// 		this._age = age;
+// 	}
+// 	get age(){
+// 		return this._age;
+// 	}
+	
+// }
+
+// class Student extends Person {
+// 	study(){
+// 		console.log(`${this._name} is studying`);
+// 	}
+// }
+
+// class Teacher extends Person {
+// 	teach(){
+// 		console.log(`${this._name} is teaching`);
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
